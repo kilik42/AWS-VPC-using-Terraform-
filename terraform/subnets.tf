8fafec897b7f74f9f3fcd34e110b0d5e4b581a9a
kilik42 <maevins@cps.edu>
2025-06-07 22:58:53 +0000

    privatezones

---
+++ terraform/subnets.tf
@@ -0,0 +1,20 @@
+resource "aws_subnet" "private_zone1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/19"
+  availability_zone = locals.zone1
+
+  tags = {
+     Name = "${local.env}-private-${local.zone1}"
+ }
+}
+
+
+resource "aws_subnet" "private_zone2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "${local.env}-private-${loacl}"
+  }
+}
+
