edd4b9f471436df4f16442cdcd9cd4484571223f
kilik42 <maevins@cps.edu>
2025-06-07 23:30:32 +0000

    working on routes

---
+++ terraform/routes.tf
@@ -0,0 +1,9 @@
+resource "aws_route_table" "private_route"{
+    vpc_id = aws_vpc.main.id
+
+    route{
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat.id
+        
+    }
+}
