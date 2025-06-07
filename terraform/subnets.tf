28608c5768426a3db6b60ae4143b0e10585e2dfc
kilik42 <maevins@cps.edu>
2025-06-07 23:07:51 +0000

    public zones

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,3 +1,6 @@
+
+#########################privatesubnets#####################
+
 resource "aws_subnet" "private_zone1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/19"
@@ -11,15 +14,23 @@ resource "aws_subnet" "private_zone1" {
 
 }
 
-
 resource "aws_subnet" "private_zone2" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.32.0/19"
+    availability_zone = locals.zone2
 
-  tags = {
-    Name = "${locals.env}-private-${locals.zone2}"
-  }
+    tags = {
+        Name = "${locals.env}-private-${locals.zone2}"
+        "kubernetes.io/role/internal-elb" = "1"
+        "kubernetes.io/cluster/${locals.env}-${locals.eks_name}" ="owned"
+    }
+
 }
 
 
+#########################public subnets#####################
 
+resource "aws_subnet" "public_zone1"{
+    vpc_id                 = aws_subnet.main.id
+
+}
