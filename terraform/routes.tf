f921191acf1c2d92918d2217a755bd5a72cc0b1d
kilik42 <maevins@cps.edu>
2025-06-07 23:33:29 +0000

    public and private routes

--- terraform/routes.tf
+++ terraform/routes.tf
@@ -4,6 +4,25 @@ resource "aws_route_table" "private_route"{
     route{
         cidr_block = "0.0.0.0/0"
         nat_gateway_id = aws_nat_gateway.nat.id
-        
+
     }
+
+    tags = {
+      Name = "${locals.env}-private"
+    }
+}
+
+resource "aws_route_table" "public"{
+    vpc_id = aws_vpc.main.id
+
+    route{
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_internet_gateway.nat.id
+
+    }
+    
+    tags={
+       Name = "${locals.env}-private" 
+    }
+
 }
